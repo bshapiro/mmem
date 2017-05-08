decfbddfe258a3ae59e21fa3022a22a8457837ef
Benj Shapiro <benj.shapiro@gmail.com>
2017-05-08 16:43:48 -0400

    added basic distribution template

---
+++ distribution.py
@@ -0,0 +1,12 @@
+
+class Distribution:
+
+    def __init__(self, samples):
+        self.samples = samples
+        self.reestimate(samples)
+
+    def reestimate(samples):
+        pass
+
+    def log_likelihood(samples):
+        pass
